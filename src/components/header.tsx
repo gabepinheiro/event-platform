e2823e033367c2e7521a3a1d0262cb0b7105e713
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-22 10:55:43 -0300

    feat(components/header): adding Logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,9 @@
+import { Logo } from './logo'
+
 export function Header() {
   return (
     <header className='w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600'>
-      <h1>Header</h1>
+      <Logo />
     </header>
   )
 }
