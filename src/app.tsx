a04ba625b5f63e10621241d554cfd453147ad43f
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-22 11:37:31 -0300

    feat(pages): adding event page

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,5 @@
-import { useLessons } from '@/lib/hooks'
-import { Header } from '@/components'
+import { Event } from '@/pages'
 
 export function App() {
-  const { lessons } = useLessons()
-
-  if (!lessons) {
-    return <p>Fetching lessons.....</p>
-  }
-
-  return (
-    <div>
-      <Header />
-      <h1>Event Platform</h1>
-      <h2>Lessons: </h2>
-      <ul>
-        {lessons.map((lesson) => (
-          <li key={lesson.id}>{lesson.title}</li>
-        ))}
-      </ul>
-    </div>
-  )
+  return <Event />
 }
---
+++ src/pages/event.tsx
@@ -0,0 +1,13 @@
+import { Header, Video, Sidebar } from '@/components'
+
+export default function Event() {
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <main className='flex flex-1'>
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
+  )
+}
