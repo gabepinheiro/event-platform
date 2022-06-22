5ddafcee3cf189af13833c455c8a93f22ab8c54c
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-22 13:51:01 -0300

    feat(component): create initial structure to lesson component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,17 @@
+import { Lesson } from '@/components'
+
 export function Sidebar() {
   return (
     <aside className='w-[348px] bg-gray-700 p-6 border-l border-gray-600'>
       Sidebar
+      <div className='flex flex-col gap-8'>
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+      </div>
     </aside>
   )
 }
