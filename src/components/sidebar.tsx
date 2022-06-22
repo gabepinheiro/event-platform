695f71732193bc6032daaa650c6046c57823f790
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-22 13:54:22 -0300

    feat(sidebar): adding heading

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,7 +3,9 @@ import { Lesson } from '@/components'
 export function Sidebar() {
   return (
     <aside className='w-[348px] bg-gray-700 p-6 border-l border-gray-600'>
-      Sidebar
+      <h3 className='font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block'>
+        Cronograma de aulas
+      </h3>
       <div className='flex flex-col gap-8'>
         <Lesson />
         <Lesson />
