26c16fd17d791f313a635a4f0d3e16b8fe0efba3
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-22 15:02:14 -0300

    feat(lib/hooks/useLessons): refactors the type to match the query data return

--- src/lib/hooks/useLessons.ts
+++ src/lib/hooks/useLessons.ts
@@ -5,14 +5,17 @@ import { GET_LESSONS_QUERY } from '@/lib/querys'
 export type Lesson = {
   id: string
   title: string
+  slug: string
+  lessonType: string
+  availableAt: string
 }
 
-export type LessonsResponseData = {
+export type QueryLessonsResponseData = {
   lessons: Lesson[]
 }
 
 export const useLessons = () => {
-  const { data, loading } = useQuery<LessonsResponseData>(GET_LESSONS_QUERY)
+  const { data, loading } = useQuery<QueryLessonsResponseData>(GET_LESSONS_QUERY)
 
   return {
     lessons: data?.lessons, loading,
