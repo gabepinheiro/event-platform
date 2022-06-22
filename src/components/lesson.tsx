b50e40f63b1c771a1f7786ce9459eaee30926d44
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-22 15:07:06 -0300

    feat(components/lesson): add properties and render dynamic content based on them

--- src/components/lesson.tsx
+++ src/components/lesson.tsx
@@ -1,23 +1,39 @@
-import { CheckCircle } from 'phosphor-react'
+import { CheckCircle, Lock } from 'phosphor-react'
 
-export function Lesson() {
+type LessonType = 'live' | 'class'
+
+type LessonProps = {
+  title: string
+  slug: string
+  availableAt: Date
+  type: LessonType
+}
+
+export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const isLessonAvailable = false
+
   return (
-    <a href='#'>
-      <span className='text-gray-300'>Terça • 22 de junho • 19h00</span>
+    <a href={slug}>
+      <span className='text-gray-300'>{availableAt.toString()}</span>
 
       <div className='rounded border border-gray-500 p-4 mt-2 '>
         <header className='flex items-center justify-between'>
-          <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
-            <CheckCircle size={20} />
-            Contéudo liberado
-          </span>
+          {isLessonAvailable ? (
+            <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
+              <CheckCircle size={20} />
+              Contéudo liberado
+            </span>
+          ) : (
+            <span className='text-sm text-orange-500 font-medium flex items-center gap-2'>
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
           <span className='text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold uppercase'>
-            Ao vivo
+            {type === 'live' ? 'Ao vivo' : 'Aula prática'}
           </span>
         </header>
-        <strong className='text-gray-200 mt-5 block'>
-          Abertura do evento Ignite Lab
-        </strong>
+        <strong className='text-gray-200 mt-5 block'>{title}</strong>
       </div>
     </a>
   )
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -7,12 +7,12 @@ export function Sidebar() {
         Cronograma de aulas
       </h3>
       <div className='flex flex-col gap-8'>
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        <Lesson
+          title='Aula 01'
+          slug='aula-01'
+          availableAt={new Date()}
+          type='live'
+        />
       </div>
     </aside>
   )
